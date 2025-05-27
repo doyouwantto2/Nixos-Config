0d82952579c1a8ef4f505675e03bcfaf92ef4628
doyouwantto2 <tinhphong2580@gmail.com>
2025-05-27 21:41:16 +0700

    use nix flake command

--- flake.nix
+++ flake.nix
@@ -5,11 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.anastasia = nixpkgs.nixosSystem {
+       system = "x86_64-linux";
+       modules = [ ./configuration.nix ];
+    };
   };
 }
