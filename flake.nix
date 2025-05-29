743055bb3c9f3769c55b95f00d6c43699dbcd776
doyouwantto2 <tinhphong2580@gmail.com>
2025-05-29 11:06:42 +0700

    try flake.nix again

--- flake.nix
+++ flake.nix
@@ -5,11 +5,17 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs }: 
+  let
+  	system = "x86_64-linux";
+	lib = nixpkgs.lib;
+  in
+  {
+  	nixosConfiguration = {
+		nixosConfig = lib.nixosSystem {
+			inherit system;
+			modules = [ ./configuration.nix ];
+		};
+	};
   };
 }
