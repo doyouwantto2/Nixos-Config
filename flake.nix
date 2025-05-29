313f466cf445f7812436b28a964796c6859d1931
doyouwantto2 <tinhphong2580@gmail.com>
2025-05-29 16:30:22 +0700

    add nixcats

--- flake.nix
+++ flake.nix
@@ -3,15 +3,24 @@
 
   inputs = {
       nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+
+      nixcats.url = "github:jordanisaacs/nixcats";
+      nixcats.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+  let 
+    system = "x86_64-linux";
+  in
+  {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
       ];
     };
+    
+    packages.${system}.nixcats = nixcats.packages.${system}.default;
   };
 }
 
